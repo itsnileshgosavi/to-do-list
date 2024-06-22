44ba32d1306a551fda4f3371a9aa278109d98da5
Nilesh <nbgosavi5@gmail.com>
2024-06-22 20:00:37 +0530

    created simple todo list add in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,58 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { todo } from './todo';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
+  standalone:true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  imports: [CommonModule, FormsModule]
 })
 export class AppComponent {
-  title = 'todo';
+  inputValue: string = '';
+  todos: todo[] = [];
+
+  
+
+  ngOnInit() {
+    // Fetch todos from localStorage on component initialization
+    this.fetchTodosFromLocalStorage();
+  }
+
+  fetchTodosFromLocalStorage() {
+    const storedTodos = localStorage.getItem("todos");
+    if (storedTodos) {
+      try {
+        this.todos = JSON.parse(storedTodos);
+      } catch (error) {
+        console.error('Error parsing todos from localStorage:', error);
+        // Handle parsing error gracefully if needed
+        this.todos = [];
+      }
+    }
+  }
+
+  saveToLocal() {
+    localStorage.setItem("todos", JSON.stringify(this.todos));
+  }
+
+  PushtoArray() {
+    if (this.inputValue.trim()) {
+      const newtodo: todo = {
+        id: Date.now(),
+        name: this.inputValue,
+        isdone: false
+      };
+      this.todos.push(newtodo);
+      this.saveToLocal(); // Save todos to localStorage after pushing new todo
+      this.inputValue = '';
+    }
+  }
+
+  deleteItem(id: number) {
+    this.todos = this.todos.filter(item => item.id !== id);
+    this.saveToLocal(); // Save todos to localStorage after deleting todo
+  }
 }
